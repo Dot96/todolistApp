8e7db196ea0f5053787a6f068d2e393268d3bf81
abrar <abraraltashlan@gmail.com>
2020-01-20 09:27:36 +0300

    Initial Commit

---
+++ todolist2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todolist2
+//
+//  Created by Abrar Ibrahim on 20/01/2020.
+//  Copyright © 2020 Abrar Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
