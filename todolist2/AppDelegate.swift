6b13e6f2f990f0a385391ee24ca752932aa04243
abrar <abraraltashlan@gmail.com>
2020-01-30 16:36:45 +0300

    Todolist

--- todolist2/AppDelegate.swift
+++ todolist2/AppDelegate.swift
@@ -77,6 +77,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
 
+let ad = UIApplication.shared.delegate as! AppDelegate
+let context = ad.persistentContainer.viewContext
--- todolist2/ViewController.swift
+++ todolist2/ViewController.swift
@@ -9,12 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var tablevie: UITableView!
+    
+    var notes = [Notes]() {
+        didSet {
+            tablevie.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tablevie.dataSource = self
+        tablevie.delegate = self
     }
-
-
+    
+    /// //get data from Coredate ( loadNote)
+    override func viewWillAppear(_ animated: Bool) {
+        loadNote()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func loadNote() {
+        do{
+            notes = try context.fetch(Notes.fetchRequest())
+        } catch {
+            print ("Error") // when fetching is Failed
+        }
+    }
 }
 
+extension ViewController: UITableViewDelegate , UITableViewDataSource {
+    // to load the save Notes to Table view
+    ///   - indexPath: the section in the table clarification where the cell place
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let note = notes[indexPath.row]
+        
+        if note.isImportent {
+            let Str = "importemt"
+            cell.textLabel?.text = " importent : [\(note.text!)]"
+        }
+        else {
+            cell.textLabel?.text = note.text
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            let delitem = notes[indexPath.row]
+            context.delete(delitem)
+            ad.saveContext()
+            do {
+                notes = try context.fetch(Notes.fetchRequest())
+            } catch {
+                print("error")
+            }
+        }
+    }
+}
